047ec89f61510c16c5d831a272af7e64df6506b5
wusimo <szw184@psu.edu>
2017-09-07 15:35:50 -0400

    initial commit

---
+++ claim/models.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Claim(models.Model):
+	insuranceCompany = models.CharField(max_length = 50,null = True)
+	# Information from page 1
+	#车主身份证
+	#identityCardFace = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+	#identityCardBottom = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+	#驾照
+	#driverLicenseFace = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+	#driverLicenseBottom = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+	#商业保险
+	#commercialInsurance = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+	#交强保险
+	#obligatoryInsurance = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+	#被保险人身份证
+	#insuredIdentityFace = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+	#insuredIdentityBottom = models.ImageField(upload_to='uploads/%Y/%m/%d/')
+
+
+
+def get_image_filename(instance, filename):
+    title = instance.post.title
+    slug = slugify(title)
+    return "post_images/%s-%s" % (slug, filename)  
+
+class Post(models.Model):
+    claim = models.ForeignKey(Claim)
+    title = models.CharField(max_length=128)
+    #body = models.CharField(max_length=400)
+
+class Images(models.Model):
+    claim = models.ForeignKey(Claim, default=None)
+    image = models.ImageField(upload_to=get_image_filename,
+                              verbose_name='Image', )
+
+class InsuranceCompany(models.Model):
+	name = models.CharField(max_length = 100)
+	phoneNumber = models.CharField(max_length = 20, blank = True)
+
